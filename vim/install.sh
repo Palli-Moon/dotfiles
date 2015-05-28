0632725c60ea38903a99e7403f2a88b3ef0df868
Palli Moon <pallimoon@gmail.com>
2015-05-28 21:05:43 +0000

    Add install script

---
+++ vim/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Installing Pathogen..."
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+echo "Installing Pathogen... Done"
+echo "Installing Color scheme..."
+mkdir ~/.vim/colors
+curl -LSso ~/.vim/colors/Revolution.vim https://raw.githubusercontent.com/CruizeMissile/Revolution.vim/master/colors/Revolution.vim
+echo "Installing Color scheme... Done"
