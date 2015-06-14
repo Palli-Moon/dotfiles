8f9c706c7be21e1fdf121078f9a45724035fb67b
Palli Moon <pallimoon@gmail.com>
2015-06-14 15:52:46 +0000

    Improve make-key.sh interface, add functionality for setting git user.email and user.name

--- github/make-key.sh
+++ github/make-key.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-if [ $# -eq 0 ]; then
-    echo "Please provide email as an argument"
+if [ $# -gt 0 ]; then
+    echo "No arguements should be provided"
     exit 1
+else
+    echo "Github keygenerator by Palli Moon"
+    echo "Enter email: "
+    read email
+    echo "Enter Github user name: "
+    read username
 fi
-echo "Creating github ssh key for \"$1\"..."
-ssh-keygen -t rsa -f ~/.ssh/id_rsa -b 4096 -C "$1"
+echo "Creating github ssh key for \"$email\"..."
+ssh-keygen -t rsa -f ~/.ssh/id_rsa -b 4096 -C "$email"
 echo "Adding ssh key to ssh agent..."
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_rsa
+echo "Setting git config email to \"$email\"..."
+git config --global user.email "$email"
+echo "Setting git config username to \"$username\"..."
+git config --global user.name "$username"
 echo "Copy this key to github:"
 echo ""
 cat ~/.ssh/id_rsa.pub
